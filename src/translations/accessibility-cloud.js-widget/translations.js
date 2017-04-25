2b450ada51b5747f7ada9c2dd41784c4a7782c07
Sebastian Felix Zappe <hypo@me.com>
2017-04-26 01:06:57 +0200

    Add  "Partially accessible with wheelchair" string

--- src/translations/accessibility-cloud.js-widget/translations.js
+++ src/translations/accessibility-cloud.js-widget/translations.js
@@ -1,7 +1,7 @@
 const translations = {
   de: {},
-  de_DE: {"Yes":"Ja","No":"Nein","Accessible with wheelchair":"Rollstuhlgerecht","Not accessible with wheelchair":"Nicht rollstuhlgerecht","No results.":"Keine Ergebnisse.","Source":"Quelle","Unknown error.":"Unbekannter Fehler.","Could not load data":"Konnte Daten nicht laden."},
-  en_US: {},
+  en_US: {"Yes":"Yes","No":"No","Accessible with wheelchair":"Accessible with wheelchair","Not accessible with wheelchair":"Not accessible with wheelchair","Partially accessible with wheelchair":"Partially accessible with wheelchair","No results.":"No results.","Source":"Source","Unknown error.":"Unknown error.","Could not load data":"Could not load data"},
+  de_DE: {"Yes":"Ja","No":"Nein","Accessible with wheelchair":"Rollstuhlgerecht","Not accessible with wheelchair":"Nicht rollstuhlgerecht","Partially accessible with wheelchair":"Teilweise rollstuhlgerecht","No results.":"Keine Ergebnisse.","Source":"Quelle","Unknown error.":"Unbekannter Fehler.","Could not load data":"Konnte Daten nicht laden."},
 };
 
 export default translations;
