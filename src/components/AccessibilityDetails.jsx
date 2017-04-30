d47017fba389a02bcb98c46b99d8cf56f94282f9
Sebastian Felix Zappe <hypo@me.com>
2017-04-30 13:30:38 +0200

    Let VoiceOver speak pauses between attributes

--- src/components/AccessibilityDetails.jsx
+++ src/components/AccessibilityDetails.jsx
@@ -5,16 +5,19 @@ import isPlainObject from 'lodash/isPlainObject';
 import humanizeString from 'humanize-string';
 import { t } from 'c-3po';
 
+
 function formatName(name: string, properties: {}): string {
   const string = properties[`${name}Localized`] || humanizeString(name);
   return string.replace(/^Rating /, '');
 }
+
 
 function formatValue(value: mixed): string {
   if (value === true) return t`Yes`;
   if (value === false) return t`No`;
   return String(value);
 }
+
 
 function FormatRating({ rating }: { rating: number }) {
   const between1and5 = Math.floor(Math.min(1, Math.max(0, rating)) * 5);
@@ -25,11 +28,13 @@ function FormatRating({ rating }: { rating: number }) {
   </span>);
 }
 
+
 function DetailsArray({ array }: { array: any[] }) {
   // eslint-disable-next-line react/no-array-index-key
   const items = array.map((e, i) => <li key={i}><AccessibilityDetails details={e} /></li>);
   return <ul className="ac-list">{items}</ul>;
 }
+
 
 function DetailsObject({ object }: { object: {} }) {
   const properties = Object.keys(object).map((key) => {
@@ -49,17 +54,27 @@ function DetailsObject({ object }: { object: {} }) {
       ];
     }
     const className = `ac-${typeof value}`;
+    const formattedValue = formatValue(value);
+    // Screen readers work better when the first letter is capitalized.
+    // If the attribute starts with a lowercase letter, there is no spoken pause
+    // between the previous attribute value and the attribute name.
+    const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+
     return [
-      <dt className={className}>{name}:</dt>,
-      <dd className={className}>{formatValue(value)}</dd>,
+      <dt className={className}>{capitalizedName}:</dt>,
+      <dd className={className} aria-label={`${formattedValue}!`}>
+        <em>{formattedValue}</em>
+      </dd>,
     ];
   });
   return <dl className="ac-group" role="treeitem">{properties}</dl>;
 }
 
+
 type Props = {
   details: any;
 };
+
 
 export default function AccessibilityDetails(props: Props) {
   const details = props.details;
