449557301a2535a086c83b6fcf52d78b578df481
Sebastian Felix Zappe <hypo@me.com>
2017-05-01 02:45:59 +0200

    Use preact correctly in lib builds

--- config/webpack.config.lib.js
+++ config/webpack.config.lib.js
@@ -97,6 +97,8 @@ module.exports = {
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
       'c-3po': locale ? 'c-3po/dist/mock' : 'c-3po',
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat',
     },
   },
 
